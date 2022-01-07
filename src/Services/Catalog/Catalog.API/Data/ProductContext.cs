c1a6e9fd7a106066ea1976ab6a882d4c29acdf89
Boppaiah-Acceleon <boppaiah@covaler.com>
2022-01-07 19:06:11 +1100

    Added in the db context
    1. responsible for connecting to the  database, define the table, and store the data if any to the products variable.
    2. Static seed class to add data to the table

---
+++ src/Services/Catalog/Catalog.API/Data/ProductContext.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class ProductContext : IProductContext
+    {
+        private readonly IConfiguration _configuration;
+        public IMongoCollection<Product> Products { get; }
+
+        public ProductContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            //create a client
+            //creates a connection with the mongoDB
+            var client = new MongoClient(_configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+
+            //getting the database
+            var database = client.GetDatabase(_configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            //ppopulate the products
+            Products = database.GetCollection<Product>(_configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+
+            //seed data 
+            ProductContextSeed.SeedData(Products);
+           
+        }
+    }
+}
