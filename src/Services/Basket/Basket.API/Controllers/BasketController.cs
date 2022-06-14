510d2ae9fe82e1b80e9ea90f11c0ba63a79421fb
boppaiah <boppaiah38@yahoo.in>
2022-06-14 17:37:24 +1000

    basket api

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,36 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository ?? throw new ArgumentNullException(nameof(basketRepository));
+        }
+
+        [HttpGet("{userName}", Name ="GetBasket")]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string userName)
+        {
+            var basket = await _basketRepository.GetBasket(userName);
+            //if the user is trying to ge the basket for the first time 
+            //we create a new shopping cart for the user else we return the
+            //existing shopping cart back to the user.
+            if(basket == null)
+            {
+                return Ok(new ShoppingCart(userName));
+            }
+            return Ok(basket);
+
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public string UserName { get; set; }
+        private List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (ShoppingCartItem item in Items)
+                {
+                    total = item.Price * item.Quantity;
+                }
+                return total;
+            }
+        }
+    }
+}
